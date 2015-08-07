04b62e5cef1802e37bb739ce9ba8e9611174ffc5
Chao-Kuei Hung <ckhung@cyut.edu.tw>
2015-08-07 11:09:35 +0800

    simplify dir structure; finish load-json

--- load-json/main.js
+++ load-json/main.js
@@ -1,4 +1,4 @@
-var _ready = { device: 0, dom: 0 };
+var _ready = { device: 0, dom: 0, data: 0 };
 // http://stackoverflow.com/questions/7432815/jquery-wait-for-multiple-complete-events
 
 function isApp() { return(document.URL.indexOf('file:///android_asset')>=0); }
@@ -17,16 +17,31 @@ document.addEventListener("DOMContentLoaded", function() {
 }, false);
 // http://stackoverflow.com/questions/9899372/pure-javascript-equivalent-to-jquerys-ready-how-to-call-a-function-when-the
 
+var _config;
+var _colorIndex = 0;
+
+d3.json("config.json", function (data) {
+    if (! data) { alert("read failed: config.json"); return; }
+    _config = data;
+    _ready.data = 1; init();
+});
+
 function init() {
-    if (! (_ready.device && _ready.dom)) return;
+    if (! (_ready.device && _ready.dom && _ready.data)) return;
     var canvas = d3.select("#display svg");
-    canvas.style("border", "1px solid blue");
+    var bg = canvas.append("rect")
+	.attr("x", 0)
+	.attr("y", 0)
+	.attr("width", canvas.attr("width"))
+	.attr("height", canvas.attr("height"))
+	.style("fill", _config.background);
     var circle = canvas.append("circle")
 	.attr("cx", canvas.attr("width")/2)
 	.attr("cy", canvas.attr("height")/2)
-	.attr("r", 30).style("fill", "#0cc");
-    d3.json("map.json", function(json) {
-	alert(json.x);
+	.attr("r", 30).style("fill", "#fff");
+    circle.on("mouseover", function () {
+	circle.transition().style("fill", _config.colors[_colorIndex]);
+	_colorIndex = (_colorIndex + 1) % _config.colors.length;
     });
 }
 
