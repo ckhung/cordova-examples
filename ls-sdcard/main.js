53aa9c1e4d8571f2664d9b57c79e93c94b2c8d2d
Chao-Kuei Hung <ckhung@cyut.edu.tw>
2015-09-04 16:38:01 +0800

    read-sdcard => ls-sdcard

--- ls-sdcard/main.js
+++ ls-sdcard/main.js
@@ -11,7 +11,6 @@
 var _ready = { device: 0, dom: 0 };
 // http://stackoverflow.com/questions/7432815/jquery-wait-for-multiple-complete-events
 
-
 document.addEventListener("deviceready", function() {
     _ready.device = 1; init();
 }, false);
@@ -22,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function printListing(listing) {
     var len = listing.length;
-    var msg = "printListing: " + len + " entries in total<br />\n";
+    var msg = "/sdcard 內含以下總共 " + len + " 個目錄或檔案<br />\n";
     for(var i=0; i<len; ++i) {
 	msg += listing[i].fullPath + "<br />\n";
     }
@@ -30,18 +29,16 @@ function printListing(listing) {
 }
 
 function readDir(fileSystem) {
-    console.log("readDir");
+    console.log("ls-sdcard: readDir");
     // var dirReader = fileSystem.root.createReader();
     window.resolveLocalFileSystemURL("file:///sdcard/", function(dir) {
-	console.log("resolveLocalFileSystemURL anon callback: ");
-	console.log(JSON.stringify(dir));
+	console.log("ls-sdcard: resolveLocalFileSystemURL processing dir = " + JSON.stringify(dir,null,4).replace(/\n/g, "\n[Console LOG]"));
 	dir.createReader().readEntries(printListing);
     });
 }
 
 function init() {
     if (! (_ready.device && _ready.dom)) return;
-    console.log("cordoofy: check point B");
     window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, readDir);
 }
 
